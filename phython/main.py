4a9f6f974ada60b7cd1028b7c72ee0062c1d5c4a
claws123456789 <ashaadiya09@gmail.com>
2024-11-14 19:58:00 +0530

    14

--- phython/main.py
+++ phython/main.py
@@ -1,3 +1,23 @@
 print("hello guys")
 print("my name is farqad")
 print("result of 3+6", 3 + 6)
+
+print("my name is farqad")
+print(17)
+print("there are 7 wonders in the world")
+print("farqad\n")
+print("python")
+
+x = 7
+y = "farqad"
+print(x)
+print(y)
+
+name = input("enter your name")
+print(name)
+
+id = int(input("enter number"))
+print(id)
+
+import keyword
+print(keyword.kwlist)
