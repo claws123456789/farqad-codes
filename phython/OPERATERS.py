86d22c1df3c0f26416e68c55a53e95775c307c62
claws123456789 <ashaadiya09@gmail.com>
2024-11-21 20:00:06 +0530

    21

---
+++ phython/OPERATERS.py
@@ -0,0 +1,25 @@
+# 78,65,34,89,25
+
+t1 = 78
+t2 = 65
+t3 = 34
+t4 = 89
+t5 = 25
+
+total = t1+t2+t3+t4+t5
+print  (total)
+
+average = 291/5
+print (average)
+
+
+math = int(input("enter your math marks"))
+hindi = int(input("enter your hindi marks"))
+english = int(input("enter your english marks"))
+computer = int(input("enter your computer marks"))
+
+ans = math+english+hindi+computer
+print (ans)
+
+per = (total/4)*100
+print (per)
