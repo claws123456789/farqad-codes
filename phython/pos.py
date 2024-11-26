2592a6d2de0e1b1934d722119e2001d5f46371f4
claws123456789 <ashaadiya09@gmail.com>
2024-11-26 20:02:23 +0530

    26

---
+++ phython/pos.py
@@ -0,0 +1,30 @@
+num = 7
+if num>0:
+  print("posetive num")
+ 
+elif num>0:
+    print("nagative num")
+
+else:
+   print("zero")
+   
+   
+cp = 100
+sp = 170
+
+amount = sp - cp  
+print(amount)
+
+if sp>cp :
+  print("profit")
+
+else:
+      print("loss")
+
+
+num=int(input("enter a number"))
+if 6%2==0:
+   print("even number")
+
+else:
+    print("odd number")
