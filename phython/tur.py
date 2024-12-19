7c423e416a2b50fc46ffac0cdec1ea804068a682
claws123456789 <ashaadiya09@gmail.com>
2024-12-19 20:00:07 +0530

    19

---
+++ phython/tur.py
@@ -0,0 +1,71 @@
+import turtle
+turtle.Screen().bgcolor("red")
+turtle.Screen().setup(500,700)
+a=turtle.Turtle()
+#for i in range(4):
+
+    #a.forward(90)
+    #a.left(90)
+#sides = 6  
+#length = 110000000000000000000000000000000000000000
+#angle = 360/sides
+#for i in range(6): 
+   # a.forward(length)
+    #a.left(angle)
+#for i in range(2):
+
+   # a .forward(100)
+    #a.right(90)
+    #a.forward(50)
+    #a.right(90)
+import turtle
+
+turtle.Screen().bgcolor("Aqua")
+
+board = turtle.Turtle()
+
+
+# first triangle for star
+
+board.forward(100) # draw base
+
+
+board.left(120)
+
+board.forward(100)
+
+
+board.left(120)
+
+board.forward(100)
+
+
+board.penup()
+
+board.right(150)
+
+board.forward(50)
+
+
+# second triangle for star
+
+board.pendown()
+
+board.right(90)
+
+board.forward(100)
+
+
+board.right(120)
+
+board.forward(100)
+
+
+board.right(120)
+
+board.forward(100)
+
+
+
+
+turtle.done()
